64e39bc253b36f26ad129469405a80739382d309
Tash <tashdevteam@gmail.com>
2023-06-25 13:51:42 +0500

    task master

--- lib/ui/pages/main_pages/focus_page.dart
+++ lib/ui/pages/main_pages/focus_page.dart
@@ -1,11 +1,8 @@
 // import 'package:app_usage/app_usage.dart';
-import 'dart:async';
 
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dnd/flutter_dnd.dart';
-import 'package:percent_indicator/circular_percent_indicator.dart';
-import 'package:syncfusion_flutter_charts/charts.dart';
 import 'package:task_master/generated/locale_keys.g.dart';
 import 'package:task_master/ui/components/custom_app_bar/custom_app_bar.dart';
 import 'package:task_master/ui/theme/app_colors.dart';
